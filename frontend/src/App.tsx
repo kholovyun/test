99c5591b881a54f91e3a1e0862b69e6e6e4209a1
kholov yunus <kholov_yun@gmail.com>
2023-06-05 20:00:59 +0600

    Added date converter

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,17 @@ import { createPost, getPosts } from './store/postsSlice'
 import { IPost } from './interfaces/IPost'
 
 function App() {
+  const formatDateTime = (dateToCon: Date): string => {
+    const date = new Date(dateToCon);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const seconds = date.getSeconds().toString().padStart(2, '0');
+  
+    return `${day}.${month}.${year} ${hours}.${minutes}.${seconds}`;
+  } 
 const dispatch: AppDispatch = useDispatch()
 const {posts, author} = useSelector((state: AppState) => {return state.posts})
 useEffect(() => {
@@ -53,7 +64,7 @@ const submiting = (e: FormEvent) =>{
                 {post.message}
               </td>
               <td>
-                {post.dateTime.toString()}
+                {formatDateTime(post.dateTime)}
               </td>
             </tr>
           )})
